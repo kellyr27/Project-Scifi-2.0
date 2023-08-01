44e644dfb246d3ce0f28b5714cceddd14691ee69
Kelly <83458976+kellyr27@users.noreply.github.com>
2023-08-01 17:44:29 +1000

    Created TV Database
    Filled with 4 TV shows

--- public/databases/lists.js
+++ public/databases/lists.js
@@ -3,6 +3,21 @@
 export const lists = [
     {
         name: 'Best Classic Scifi Movies',
-        description: 'The best science fiction movies released before 1990.'
+        description: 'The best science fiction movies released before 1990.',
+        type: 'movies',
+        list: [1,2,3]
+    },
+    {
+        name: 'Best Scifi TV Shows of All Time',
+        description: 'The best science fiction tv series of all time.',
+        type: 'tv',
+        list: []
     }
 ]
+
+// {
+//     name: '',
+//     description: '',
+//     type: '',
+//     list: []
+// }
---
+++ public/databases/tvShows.js
@@ -0,0 +1,64 @@
+/**
+
+ */
+
+export const tvShows = [
+    {
+        id: 1,
+        title: 'Futurama',
+        imdbRating: 8.5,
+        imdbLink: 'https://www.imdb.com/title/tt0149460/',
+        numOfSeasons: 8,
+        numOfEpisodes: 160,
+        runtime: '22',
+        releaseYears: '1999 - 2023',
+        genres: ["Animation", "Comedy", "Science Fiction"],
+        description: `Futurama is an animated sci-fi sitcom set in the 31st century. Follow the misadventures of Fry, a pizza delivery boy who wakes up after a thousand-year cryogenic sleep, joining a crew of eccentric aliens and robots as they explore the bizarre, humorous, and satirical aspects of the future.`,
+    }, {
+        id: 2,
+        title: 'Black Mirror',
+        imdbRating: 8.7,
+        imdbLink: 'https://www.imdb.com/title/tt2085059/',
+        numOfSeasons: 6,
+        numOfEpisodes: 27,
+        runtime: '40 - 89',
+        releaseYears: '2011 - 2023',
+        genres: ["Science Fiction", "Mystery", "Drama", "Thriller", "TV Movie"],
+        description: `Black Mirror is a gripping anthology series that delves into the dark and dystopian side of modern society and technology. Each standalone episode presents thought-provoking and unsettling stories, exploring the potential consequences of our obsession with innovation and the eerie implications of a technologically advanced future.`,
+    }, {
+        id: 3,
+        title: 'Stranger Things',
+        imdbRating: 8.7,
+        imdbLink: 'https://www.imdb.com/title/tt4574334/',
+        numOfSeasons: 4,
+        numOfEpisodes: 34,
+        runtime: '42 - 139',
+        releaseYears: '2016 - 2024',
+        genres: ["Science Fiction", "Mystery", "Drama", "Fantasy"],
+        description: `Stranger Things is a thrilling sci-fi horror series set in the 1980s. When a young boy vanishes in a small town, his friends encounter a mysterious girl with supernatural powers. As they search for him, they unravel government secrets and face terrifying creatures from an alternate dimension, forging unbreakable bonds.`,
+    }, {
+        id: 4,
+        title: 'The X-Files',
+        imdbRating: 8.6,
+        imdbLink: 'https://www.imdb.com/title/tt0106179/',
+        numOfSeasons: 11,
+        numOfEpisodes: 217,
+        runtime: '43',
+        releaseYears: '1993 - 2018',
+        genres: ["Drama", "Mystery", "Science Fiction", "Thriller"],
+        description: `The X-Files is a captivating sci-fi drama following FBI agents Mulder and Scully as they investigate paranormal and unsolved cases. Combining elements of conspiracy, extraterrestrial encounters, and the unexplained, the duo's relentless pursuit of truth leads them to confront government cover-ups and unworldly phenomena, challenging their beliefs and sanity.`,
+    } 
+]
+
+// {
+//     id: 1,
+//     title: '',
+//     imdbRating: 0.0,
+//     imdbLink: '',
+//     numOfSeasons: 0,
+//     numOfEpisodes: 0,
+//     runtime: '',
+//     releaseYears: '',
+//     genres: [],
+//     description: ``,
+// }, 
