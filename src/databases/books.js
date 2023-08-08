f1bd238ce8a2ed8b6389741659b92322a01e012b
Kelly <83458976+kellyr27@users.noreply.github.com>
2023-08-08 14:40:16 +1000

    Working on book database

--- src/databases/books.js
+++ src/databases/books.js
@@ -9,94 +9,417 @@
 
 export const books = {
     0: {
-        id: 1,
-        title: 'Bone',
         titles: ['Bone'],
-        type: 2,
         author: 'Jeff Smith',
         releaseYears: '1991 - 2004',
+        genres: [],
         pages: 1332,
-        numOfBooks: 9,
-        goodreadsLink: 'https://www.goodreads.com/book/show/21458853-bone-collection',
         description: `Bone is a beloved comic book series created by Jeff Smith. This epic fantasy-adventure follows the three Bone cousins - Fone, Phoney, and Smiley - as they get lost in a mysterious valley. Amidst a war between humans and creatures, they uncover ancient secrets, friendship, and courage on their extraordinary journey.`,
         coverImages: ["/img/B01KN1YXE4.01._SCLZZZZZZZ_SX500_.jpg"]
     }, 
     1: {
-        id: 2,
-        title: null,
         titles: ['Nimona'],
-        type: 1,
         author: 'N.D. Stevenson',
         releaseYears: '2015',
+        genres: [],
         pages: 272,
-        numOfBooks: null,
-        goodreadsLink: 'https://www.goodreads.com/book/show/19351043-nimona',
         description: `Nimona is a captivating graphic novel by Noelle Stevenson. The story revolves around the quirky and shapeshifting Nimona, who becomes the sidekick of the villainous Lord Ballister Blackheart. As they challenge the kingdom's heroes, the duo uncovers dark secrets, blurring the lines between good and evil in this heartwarming and action-packed tale.`,
         coverImages: ["/img/81KfE8r5bNL.jpg"]
     }, 
     2: {
-        id: 3,
-        title: 'The Foundation Trilogy',
         titles: ['Foundation', 'Foundation and Empire', 'Second Foundation'],
-        type: 4,
         author: 'Isaac Asimov',
         releaseYears: '1951 - 1953',
+        genres: [],
         pages: 703,
-        numOfBooks: 3,
-        goodreadsLink: 'https://www.goodreads.com/book/show/50399538-the-foundation-trilogy-foundation',
         description: `The Foundation Trilogy, written by Isaac Asimov, is a groundbreaking science fiction saga. Spanning centuries, it follows mathematician Hari Seldon's plan to preserve knowledge and shorten a looming dark age. As empires rise and fall, the epic explores the power of foresight, psychohistory, and the quest to shape the future.`,
         coverImages: ["/img/374166-L.jpg", "/img/10983283-L.jpg", "/img/12884264-L.jpg", "/img/14322820-L.jpg"]
     }, 
     3: {
-        id: 5,
-        title: null,
         titles: ['The Demolished Man', 'The Stars My Destination'],
-        type: 5,
         author: 'Alfred Bester',
         releaseYears: '1953 - 1956',
+        genres: [],
         pages: null,
-        numOfBooks: 2,
-        goodreadsLink: 'https://www.goodreads.com/author/show/10992.Alfred_Bester',
         description: `Alfred Bester's books are visionary classics of science fiction. With works like "The Stars My Destination" and "The Demolished Man," he blends action, intrigue, and mind-bending concepts. Bester's writing explores psychic abilities, societal complexities, and the human psyche, leaving a lasting impact on the genre and captivating readers with his imaginative narratives.`,
         coverImages: [""]
     }, 
     4: {
-        id: 6,
-        title: null,
         titles: ['The Martian Chronicles', 'The Illustrated Man'],
-        type: 5,
         author: 'Ray Bradbury',
         releaseYears: '1950 - 1951',
+        genres: [],
         pages: null,
-        numOfBooks: 2,
-        goodreadsLink: 'https://www.goodreads.com/author/show/1630.Ray_Bradbury',
         description: `Ray Bradbury's books are timeless treasures of speculative fiction. His works, including "Fahrenheit 451," "The Martian Chronicles," and "Something Wicked This Way Comes," masterfully blend imagination and social commentary. With lyrical prose, Bradbury explores dystopian worlds, interplanetary adventures, and the essence of human nature, enchanting readers with his evocative storytelling.`,
         coverImages: [""]
     }, 
     5: {
-        id: 7,
-        title: null,
         titles: ['The Time Machine', 'The War of the Worlds'],
-        type: 5,
         author: 'H. G. Wells',
         releaseYears: '1895 - 1898',
+        genres: [],
         pages: null,
-        numOfBooks: 2,
-        goodreadsLink: 'https://www.goodreads.com/author/show/880695.H_G_Wells',
         description: `H.G. Wells' books are pioneering classics of science fiction and speculative fiction. With iconic works like "The War of the Worlds," "The Time Machine," and "The Invisible Man," Wells envisioned extraordinary worlds and technological marvels, exploring themes of alien invasion, time travel, and societal implications that continue to captivate readers across generations.`,
+        coverImages: [""]
+    },
+    6: {
+        titles: ['Paper Girls'],
+        author: 'Brian K. Vaughan',
+        releaseYears: '2015 - 2019',
+        genres: [],
+        pages: 800,
+        description: ``,
+        coverImages: [""]
+    },
+    7: {
+        titles: ['Descender'],
+        author: 'Jeff Lemire',
+        releaseYears: '2015 - 2018',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    8: {
+        titles: ['Y: The Last Man'],
+        author: 'Brian K. Vaughan',
+        releaseYears: '2002 - 2008',
+        genres: [],
+        pages: 1440,
+        description: ``,
+        coverImages: [""]
+    },
+    9: {
+        titles: ['Planetes'],
+        author: 'Makoto Yukimura',
+        releaseYears: '1999 - 2004',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    10: {
+        titles: ['2001 Nights'],
+        author: 'Yukinobu Hoshino',
+        releaseYears: '1984 - 1986',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    11: {
+        titles: ['Akira'],
+        author: 'Katsuhiro Otomo',
+        releaseYears: '1982 - 1990',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    12: {
+        titles: ['The Walking Dead'],
+        author: 'Robert Kirkman',
+        releaseYears: '2003 - 2019',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    13: {
+        titles: ['Transmetropolitan'],
+        author: 'Warren Ellis',
+        releaseYears: '1997 - 2002',
+        genres: ['Cyberpunk', 'Science Fiction', 'Black Comedy'],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    14: {
+        titles: ['Blame!'],
+        author: 'Tsutomu Nihei',
+        releaseYears: '1997 - 2003',
+        genres: ['Action','Cyberpunk','Post-Apocalyptic'],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    15: {
+        titles: ['Saga'],
+        author: 'Brian K. Vaughan',
+        releaseYears: '2012 - ',
+        genres: ['Epic Space Opera','Fantasy'],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    16: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    17: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    18: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    19: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    20: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    21: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    22: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    23: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    24: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    25: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    27: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    28: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    29: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    30: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    31: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    32: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    33: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    34: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    35: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    36: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    37: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    38: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    39: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    40: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    41: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    42: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    43: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    44: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    45: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
+        coverImages: [""]
+    },
+    46: {
+        titles: [],
+        author: '',
+        releaseYears: '',
+        genres: [],
+        pages: null,
+        description: ``,
         coverImages: [""]
     }
 }
-
-// {
-//     id: null,
-//     title: null,
-//     titles: null,
-//     type: null,
-//     author: null,
-//     releaseYears: null,
-//     pages: null,
-//     numOfBooks: null,
-//     goodreadsLink: null,
-//     description: ``
-// }
